784f8a9ebe1a1d997fa15d4b7d0b918b25825cb9
bsg <601063434@qq.com>
2021-12-31 13:34:08 +0800

    读写Json

---
+++ Assets/Script/BSGJsonExtension.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class BSGJsonExtension
+{
+    //使用方法
+    //读取
+    //readStudentData = BSGJsonExtension.BSGReadJson<StudentData>(flie, creatStudentData);
+    //creatStudentData 没有json文件时候写入的初始值,也可以不传,生成空白的json文件
+    //存储  实现原理是:新生成json覆盖老的json   
+    //BSGJsonExtension.BSGSaveJson<StudentData>(flie, readStudentData);
+
+    /// <summary>
+    /// 读取Json
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="file">路径</param>
+    /// <param name="defaultInfo">无Json初始值</param>
+    /// <param name="fileNotFoundException">无Json事件</param> 
+    /// <returns></returns>
+    public static T BSGReadJson<T>(string file, T defaultInfo = default(T), System.Action fileNotFoundException = null)  
+    {
+        var url = Application.persistentDataPath + "/" + file;
+        Debug.Log("读取路径" + url);
+        if (File.Exists(url))
+        {
+            string sr = null;
+            try
+            {
+                sr = File.ReadAllText(url);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError("读取出错" + e.Message);
+                return default(T);
+            }
+            Debug.Log("读取Json成功");
+            //转换成Json格式      
+            T tempT = JsonUtility.FromJson<T>(sr.ToString());
+            Debug.Log(sr);
+            return tempT;
+        }
+        else
+        {
+            Debug.Log("Json文件不存在");
+            if (fileNotFoundException != null)
+            {
+                fileNotFoundException.Invoke();
+            }
+            T s = System.Activator.CreateInstance<T>();
+            s = defaultInfo;
+            //创建json文件
+            BSGCreatJson<T>(file, s);
+            return s;
+        }
+    }
+
+
+    public static bool BSGSaveJson<T>(string file, T info)
+    {
+        var url = Application.persistentDataPath + "/" + file;
+        Debug.Log("开始保存Json" + url);
+        FileInfo fileInfo = new FileInfo(url);
+        StreamWriter sw = fileInfo.CreateText();
+        string json = JsonUtility.ToJson(info, true);
+        try
+        {
+            sw.WriteLine(json.ToString());
+        }
+        catch (System.Exception e)
+        {
+
+            Debug.Log("保存Json失败" + e.Message);
+            sw.Close();
+            return false;
+        }
+        Debug.Log("保存Json成功");
+        sw.Close();
+        return true;
+    }
+
+    //创建json文件
+    private static T BSGCreatJson<T>(string file, T defaultInfo)
+    {
+        var url = Application.persistentDataPath + "/" + file;
+        Debug.Log("开始创建Json" + url);
+        FileInfo fileInfo = new FileInfo(url);
+        StreamWriter sw = fileInfo.CreateText();
+        string json = JsonUtility.ToJson(defaultInfo, true);
+        try
+        {
+            sw.WriteLine(json.ToString());
+        }
+        catch (System.Exception e)
+        {
+
+            Debug.Log("创建Json失败" + e.Message);
+            sw.Close();
+            return defaultInfo;
+        }
+        Debug.Log("创建Json成功" + json);
+        sw.Close();
+        return defaultInfo;
+    }
+
+
+}
+
+
---
+++ Assets/Script/TestJson.cs
@@ -0,0 +1,114 @@
+﻿using LitJson;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class TestJson : MonoBehaviour
+{
+    string flie = "jsontest.json";  
+
+    public StudentData creatStudentData;
+    public StudentData readStudentData;
+    public InputField inputField;
+    // Use this for initialization
+    void Start()
+    {
+        InitStudentData();
+    }
+
+    public void BtnRead()
+    {
+        //有个小遗憾   修改readData 时候 会同时修改 creatStudent  
+        flie = inputField.text;
+        readStudentData = BSGJsonExtension.BSGReadJson<StudentData>(flie, creatStudentData);
+    }
+    public void BtnSet()
+    {
+        for (int i = 0; i < readStudentData.classes.Length; i++)
+        {
+            readStudentData.classes[i].className = "修改班级" + i;
+        }
+        BSGJsonExtension.BSGSaveJson<StudentData>(flie, readStudentData);
+    }
+    /// <summary>
+    /// 初始化Data
+    /// </summary>
+    public void InitStudentData()
+    {
+        if (creatStudentData.classes.Length == 0)
+        {
+            Debug.Log("初始化Data");
+            creatStudentData.classes = new Class[3];
+            for (int i = 0; i < creatStudentData.classes.Length; i++)
+            {
+                creatStudentData.classes[i].className = "班级" + i;
+                creatStudentData.classes[i].studentInfos = new StudentInfo[10];//每个班级10个人
+                for (int j = 0; j < creatStudentData.classes[i].studentInfos.Length; j++)
+                {
+                    creatStudentData.classes[i].studentInfos[j].studentName = "张" + j;
+                    creatStudentData.classes[i].studentInfos[j].scoreInfos = new ScoreInfo[5];
+                    for (int k = 0; k < creatStudentData.classes[i].studentInfos[j].scoreInfos.Length; k++)
+                    {
+                        creatStudentData.classes[i].studentInfos[j].scoreInfos[k].number = k;
+                        creatStudentData.classes[i].studentInfos[j].scoreInfos[k].Score1 = Random.Range(0, 100);
+                        creatStudentData.classes[i].studentInfos[j].scoreInfos[k].Score2 = Random.Range(0, 100);
+                        creatStudentData.classes[i].studentInfos[j].scoreInfos[k].Score3 = Random.Range(0, 100);
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+/// <summary>
+/// 学生数据 
+/// </summary>
+[System.Serializable]
+public struct StudentData //学生信息 
+{
+    public Class[] classes; //多个班级
+}
+/// <summary>
+/// 班级
+/// </summary>
+[System.Serializable]
+public struct Class
+{
+    public string className; //班级名
+    public StudentInfo[] studentInfos;  //学生信息
+}
+/// <summary>
+/// 学生信息
+/// </summary>
+[System.Serializable]
+public struct StudentInfo
+{
+    public string studentName;  //学生名
+    public ScoreInfo[] scoreInfos; //学生多次分数 
+}
+/// <summary>
+/// 分数
+/// </summary>
+[System.Serializable]
+public struct ScoreInfo
+{
+    public int number;//次数  
+    public int Score1;//分数1
+    public int Score2;//分数2  
+    public int Score3;//分数3 
+}
+
